14dd260d326bfb302f0a1143e641e96b9337b068
admin <worapon.p@hobbiz.co.th>
2017-06-23 16:45:31 +0700

    get request.body

--- config/express.js
+++ config/express.js
@@ -29,10 +29,10 @@ module.exports = function (app) {
 
   // bodyParser should be above methodOverride
   app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+  app.use(bodyParser.urlencoded());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(methodOverride((req) => {
-    console.log(req.body);
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
       // look in urlencoded POST bodies and delete it
       const method = req.body._method;
